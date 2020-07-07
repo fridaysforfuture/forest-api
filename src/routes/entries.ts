622cc6ec752540e960edc4b27e2d2b48123a621d
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-07-07 18:16:13 +0200

    Changed username to sub

--- src/routes/entries.ts
+++ src/routes/entries.ts
@@ -20,59 +20,58 @@ function testParams(body: any) {
 }
 
 router.use(BodyParser.json());
-router.put('/:name', needAuth,
-  async (request, response) => {
-    if(request.body.links == undefined) {
-      request.body.links = [];
-    }
-    if(request.body.socialLinks == undefined) {
-      request.body.socialLinks = {};
-    }
-    if(request.body.friendlyName == undefined) {
-      request.body.friendlyName = request.params.name
-    }
-    if(typeof(request.user!.username) !== 'string') {
-      response.status(401);
-      response.send({ error: "Token does not have string username claim" });
-      return;
-    }
-    try
-    {
-      testParams(request.body);
-    }
-    catch (error)
-    {
-      response.status(400);
-      response.send({ error });
-      return;
-    }
-    
-    const entry = await Entry.findOne( {
-        name: request.params.name.toLowerCase(),
-      });
-    if(entry === null) {
-      new Entry({
-        name: request.params.name,
-        links: request.body.links,
-        socialLinks: request.body.socialLinks,
-        friendlyName: request.body.friendlyName,
-        owner: request.user!.username
-      }).save();
-      response.send({});
-      return;
-    }
-    if(entry.owner !== request.user!.username) {
-      response.status(401);
-      response.send({
-        error: "Entry already exists and is owned by different user"
-      });
-      return
-    }
-    entry.links = request.body.links;
-    entry.friendlyName = request.body.friendlyName;
-    entry.save();
-    response.send({});
+
+router.put('/:name', needAuth, async (request, response) => {
+  if (request.body.links == undefined) {
+    request.body.links = [];
+  }
+  if (request.body.socialLinks == undefined) {
+    request.body.socialLinks = {};
+  }
+  if (request.body.friendlyName == undefined) {
+    request.body.friendlyName = request.params.name;
+  }
+  if (typeof request.user!.sub !== 'string') {
+    response.status(401);
+    response.send({ error: 'Token does not have string sub claim' });
+    return;
+  }
+  try {
+    testParams(request.body);
+  } catch (error) {
+    response.status(400);
+    response.send({ error });
+    return;
+  }
+  // We only want lower case entries
+  request.params.name = request.params.name.toLowerCase();
+
+  const entry = await Entry.findOne({
+    name: request.params.name,
   });
+  if (entry === null) {
+    new Entry({
+      name: request.params.name,
+      links: request.body.links,
+      socialLinks: request.body.socialLinks,
+      friendlyName: request.body.friendlyName,
+      owner: request.user.sub,
+    }).save();
+    response.send({});
+    return;
+  }
+  if (entry.owner !== request.user.sub) {
+    response.status(401);
+    response.send({
+      error: 'Entry already exists and is owned by different user',
+    });
+    return;
+  }
+  entry.links = request.body.links;
+  entry.friendlyName = request.body.friendlyName;
+  entry.save();
+  response.send({});
+});
 
 router.patch('/:name', needAuth,
   async (request, response) => {
@@ -98,7 +97,11 @@ router.patch('/:name', needAuth,
     });
     return;
   }
+<<<<<<< HEAD
   if(entry.owner !== request.user!.username) {
+=======
+  if (entry.owner !== request.user.sub) {
+>>>>>>> 785b5df... Changed username to sub
     response.status(401);
     response.send({
         error: "Entry is owned by different user",
